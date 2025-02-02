8867619b9b9a4cf00b3719135e9146f097baa6fc
Wesley <wehaagmans@gmail.com>
2025-02-02 01:14:50 +0100

    feat: :sparkles: Integrate Swagger for API documentation setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+import type { INestApplication } from '@nestjs/common';
+
+function swaggerSetup(app: INestApplication) {
+  const SWAGGER_API_PATH = 'swagger';
+  const config = new DocumentBuilder()
+    .setTitle('Meeting Wasted Calculator')
+    .setDescription('Backend for meeting wasted calculator')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(SWAGGER_API_PATH, app, documentFactory);
 }
-bootstrap();
+
+async function serverSetup() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ?? 3000;
+
+  swaggerSetup(app);
+
+  await app.listen(+port);
+}
+
+void serverSetup();
