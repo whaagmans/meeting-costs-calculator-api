1bb5b8a87ed70274aed14b914d8f71e40c8f4cb1
Wesley <wehaagmans@gmail.com>
2025-02-16 01:17:45 +0100

    feat: :sparkles: Integrate RoomsModule into AppModule for room management

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
+import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
-  imports: [],
+  imports: [RoomsModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
