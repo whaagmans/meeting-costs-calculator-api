332b43ffa01a1f77335a87766e66147f01d5e8c0
Wesley <wehaagmans@gmail.com>
2025-02-17 18:15:41 +0100

    feat: :sparkles: Update getHello method to getWelcome with a welcome message

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getWelcome(): { message: string } {
+    return { message: 'Welcome to the Meeting Costs Calculator API' };
+  }
   }
 }
