9fa9f7aca3605f242b71661536c0dc8c863e72d7
Wesley <wehaagmans@gmail.com>
2025-02-24 01:45:47 +0100

    feat(types): :label: Update CreateUserDto and UpdateUserDto with validation rules and salary information

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,54 @@
-export class CreateUserDto {}
+import { Type } from 'class-transformer';
+import {
+  IsAlphanumeric,
+  IsEnum,
+  IsInt,
+  IsISO4217CurrencyCode,
+  IsNotEmpty,
+  IsOptional,
+  IsPositive,
+  IsString,
+  IsStrongPassword,
+  MaxLength,
+  MinLength,
+  ValidateNested,
+} from 'class-validator';
+import { PaymentInterval } from '../users.interface';
+class SalaryDto {
+  @IsNotEmpty()
+  @IsPositive()
+  amount: number;
+
+  @IsNotEmpty()
+  @IsEnum(PaymentInterval)
+  paymentInterval: PaymentInterval;
+
+  @IsInt()
+  @IsNotEmpty()
+  @IsPositive()
+  workedHoursPerWeek: number;
+
+  @IsISO4217CurrencyCode()
+  @IsOptional()
+  currency?: string;
+}
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsAlphanumeric()
+  @MinLength(3)
+  @MaxLength(20)
+  username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(50)
+  displayName: string;
+
+  @IsStrongPassword()
+  password: string;
+
+  @ValidateNested()
+  @Type(() => SalaryDto)
+  salaryInfo: SalaryDto;
+}
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { IsUUID } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsUUID()
+  id: string;
+}
