99f1565dab03f87c839bb6b7d07af122e8ddc5be
Wesley <wehaagmans@gmail.com>
2025-02-24 03:13:27 +0100

    feat(types): :label: Update UpdateUserDto to make salaryPerSecond optional

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,8 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
-import { IsPositive } from 'class-validator';
+import { IsOptional, IsPositive } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsOptional()
   @IsPositive()
-  salaryPerSecond: number;
+  salaryPerSecond?: number;
 }
