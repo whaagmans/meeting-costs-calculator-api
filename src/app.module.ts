4669cc7825bcd9938d8577b8f3e67b1f90a4625c
Wesley <wehaagmans@gmail.com>
2025-03-29 18:03:54 +0100

    feat(server): :sparkles: add HttpModule to the application imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { RoomsModule } from './rooms/rooms.module';
 import { UsersModule } from './users/users.module';
 import { TerminusModule } from '@nestjs/terminus';
 import { PrismaService } from './config/prisma/prisma.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [RoomsModule, UsersModule, TerminusModule],
+  imports: [RoomsModule, UsersModule, TerminusModule, HttpModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
